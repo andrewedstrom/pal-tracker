36ae2d30a5d92ee4be44c3f9d4dbecdf27796b07
Ramkumar Vengadakrishnan <rvengadakrishnan@pivotal.io>
2018-05-30 14:27:01 -0700

    Update dependencies
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class PalTrackerApplication
+class PalTrackerApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(PalTrackerApplication::class.java, *args)
